4c0fbd1c93f4ba5da5acd7b0514f6f1dce88afc9
Michael Hearn <git@mikehearn.net>
2018-04-27 17:55:49 -0400

    Add credentials: include to browserId fetch

--- addon/services/session.js
+++ addon/services/session.js
@@ -80,7 +80,7 @@ function reportBrowserId(knownId) {
 }
 
 function getBrowserId() {
-  return fetch(config.etagAPI)
+  return fetch(config.etagAPI, {credentials: 'include'})
     .then(checkStatus)
     .then(response => response.json());
 }
