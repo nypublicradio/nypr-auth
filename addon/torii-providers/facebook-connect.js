202d837b30079fc8f052c0b32a33d43a470a8d9f
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2019-04-04 11:45:19 -0400

    add fastboot compatibility

--- addon/torii-providers/facebook-connect.js
+++ addon/torii-providers/facebook-connect.js
@@ -1,7 +1,17 @@
 import FacebookConnectProvider from 'torii/providers/facebook-connect';
+import { inject as service } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 export default FacebookConnectProvider.extend({
-  fetch(data) {
-    return data;
+  fastboot: service(),
+  isFastboot: reads('fastboot.isFastBoot'),
+
+  init(){
+    if (typeof document === 'undefined') {
+      // don't run in fastboot
+      return;
+    }
+    this._super(...arguments);
   }
+
 });
