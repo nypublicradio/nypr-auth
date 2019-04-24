e0714afd36ecabb25c159a5ebb3beae0b90c80ed
Jeff Keen <jeff@keen.me>
2019-04-24 13:27:48 -0500

    Update facebook-connect provider to be fastboot compatible

--- addon/torii-providers/facebook-connect.js
+++ addon/torii-providers/facebook-connect.js
@@ -1,7 +1,16 @@
 import FacebookConnectProvider from 'torii/providers/facebook-connect';
+import { inject as service } from '@ember/service';
+import { reads } from '@ember/object/computed';
+import { get } from '@ember/object';
 
 export default FacebookConnectProvider.extend({
+  fastboot: service(),
+  isFastboot: reads('fastboot.isFastboot'),
+
   fetch(data) {
+    if(get(this, 'isFastboot')) {
+      return;
+    }
     return data;
   }
 });
