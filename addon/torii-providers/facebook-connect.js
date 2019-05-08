13562ea551048d3d8d3e17db5a322796234d2c35
Brian Whitton <me@brianwhitton.com>
2019-05-08 14:32:13 -0400

    no need to inject service here
     the typeof document check makes this safe enough

--- addon/torii-providers/facebook-connect.js
+++ addon/torii-providers/facebook-connect.js
@@ -1,10 +1,6 @@
 import FacebookConnectProvider from 'torii/providers/facebook-connect';
-import { inject as service } from '@ember/service';
-import { reads } from '@ember/object/computed';
 
 export default FacebookConnectProvider.extend({
-  fastboot: service(),
-  isFastboot: reads('fastboot.isFastBoot'),
 
   init(){
     if (typeof document === 'undefined') {
